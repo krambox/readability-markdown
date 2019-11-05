d8cc3cceec9a88b285250ff3f8e529d76120bd74
Kai Kramer <kai.kramer@empolis.com>
2019-11-05 14:26:26 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+var fs = require('fs')
+var argv = require('minimist')(process.argv.slice(2))
+var read = require('node-read')
+var TurndownService = require('turndown')
+var turndownService = new TurndownService()
+
+console.dir(argv)
+
+var data = fs.readFileSync(argv._[0])
+
+read(data.toString(), function(err, article, res) {
+  // Title
+  console.log(article.title)
+
+  // Main Article.
+  //console.log(article.content)
+
+  var markdown = turndownService.turndown(article.content)
+
+  console.log(markdown)
+
+  // HTML
+  //console.log(article.html)
+
+  // DOM
+  //console.log(article.dom)
+})
